93a4d48bd2b6637a970022e1f1e262aa4ea452ea
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-08-08 23:33:03 -0400

    try catch implemented

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,9 @@
+'use strict'
+
 const sequelize = require('../connection.js');
-const User = require('../model/user.js');
+const User = require('../model/User.js');
 const { hash } = require('../helpers/hash.js');
+
 
 const UsersController = {
 
@@ -11,18 +14,41 @@ const UsersController = {
     },
     
     create: async function(params) {
-        
-        const new_user = await User.create({
-            name: params.name,
-            lastname: params.lastname,
-            email: params.email,
-            password: await hash(params.password, 10)
-        });
-    
-        new_user.name = "Jesus";
-        await new_user.save();
 
-        return "Agregado con existo";
+        try {
+            const new_user = await User.create({
+                name: params.name,
+                lastname: params.lastname,
+                email: params.email,
+                password: await hash(params.password, 10).catch(err => {throw new Error('Debes ingresar una contraseña')})
+            });
+        
+            new_user.name = "Jesus";
+            await new_user.save();
+    
+            return "Agregado con exito";
+        
+        } catch (error) {
+            let messages = [];
+            
+            // si hay multiples errores
+            if( error.errors != undefined) {
+                
+                error.errors.forEach(err => {
+                    messages.push(err.message);
+                });
+
+            // en caso que solo haya un error
+            }else {
+                messages.push( error.message );
+            }
+
+
+
+
+            return messages;
+        }
+
     },
     
     show: function(id) {},
