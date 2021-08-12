6cca0891505dd3e9c8f7c6ad1fd2459afd92c49c
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-08-11 20:56:26 -0400

    department model

---
+++ src/models/Department.js
@@ -0,0 +1,30 @@
+const {DataTypes } = require("sequelize");
+const sequelize = require('../connection.js');
+
+const Department = sequelize.define("departments", {
+    id: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {            
+            notNull: {
+                args: true,
+                msg: "El nombre del departamento no es valido"
+            },
+            notEmpty: {
+                args: true,
+                msg: "Debes ingresar el nombre del departamento"
+            }
+        }
+    },
+});
+
+(async () => {
+  await sequelize.sync();
+})();
+
+module.exports = Department;
